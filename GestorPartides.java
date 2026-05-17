6e0917295b20cdae51033d80df36bcd4a8ff8139
Pere Miquel <peremiquel777@gmail.com>
2026-05-17 21:19:33 +0200

    Nova classe GestorPartides amb Guardar i carregar partida

---
+++ GestorPartides.java
@@ -0,0 +1,28 @@
+package Principi.Reptes.Rummy;
+
+import Principi.Reptes.Rummy.Jocs.JocBase;
+
+import java.io.*;
+
+public class GestorPartides {
+    private static final String DIRECTORI = "partides_guardades/";
+
+    public static void guardarPartida(JocBase joc, String nomFitxer) {
+        new File(DIRECTORI).mkdirs();
+        try (ObjectOutputStream oos = new ObjectOutputStream( new FileOutputStream(DIRECTORI + nomFitxer + ".dat" ))) {
+            oos.writeObject(joc);
+            System.out.println("Partida guardada correctament.");
+        } catch (IOException e) {
+            System.out.println("Error al guardar partida: " + e.getMessage());
+        }
+    }
+
+    public static JocBase carregarPartida(String nomFitxer) {
+        try (ObjectInputStream ois = new ObjectInputStream( new FileInputStream(DIRECTORI + nomFitxer + ".dat" ))) {
+            return (JocBase) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al carregar partida: " + e.getMessage());
+            return null;
+        }
+    }
+}
